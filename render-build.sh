e23abb5e3bd14b4ae93056eecf3a304eef47bb95
isabelperez-bold <isabel.perez@bold.co>
2026-04-15 14:36:45 -0500

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -7,14 +7,18 @@ npm install
 echo "2. Instalando Chrome para Puppeteer..."
 npx puppeteer browsers install chrome
 
-echo "3. Configurando fuentes de Emojis en la ruta moderna..."
-# Usamos la ruta estándar actual de Linux para fuentes de usuario
+echo "3. Configurando fuentes (Emojis y Montserrat)..."
 mkdir -p ~/.local/share/fonts
 
-# Descargamos la fuente
+# Descargamos Noto Emoji
 curl -L -o ~/.local/share/fonts/NotoColorEmoji.ttf https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
 
-# Actualizamos la caché
+# Descargamos Montserrat (Versión Variable) directamente al servidor
+curl -L -o ~/.local/share/fonts/Montserrat-VariableFont_wght.ttf https://github.com/JulietaUla/Montserrat/raw/master/fonts/variable/Montserrat-Italic-VariableFont_wght.ttf
+curl -L -o ~/.local/share/fonts/Montserrat-Regular.ttf https://github.com/JulietaUla/Montserrat/raw/master/fonts/ttf/Montserrat-Regular.ttf
+curl -L -o ~/.local/share/fonts/Montserrat-Bold.ttf https://github.com/JulietaUla/Montserrat/raw/master/fonts/ttf/Montserrat-Bold.ttf
+
+# Actualizamos la caché de fuentes del sistema Linux
 fc-cache -f -v
 
 echo "¡Construcción finalizada con éxito!"
